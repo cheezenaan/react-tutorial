eb42a9bf5d44dd71be3bce136b155d2f53fcf75a
cheezenaan <cheezenaan@gmail.com>
2017-05-21 12:30:23 +0900

    Refactor board to make squares by using two loops

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import Square from './square';
+import { ROWS, COLUMNS } from '../config';
 
 export default class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
     );
   }
 
+  renderRows(i) {
+    const squares = [];
+    const start = ROWS * i;
+    for (let j = start; j < start + COLUMNS; j += 1) {
+      squares.push(this.renderSquare(j));
+    }
+    return (
+      <div key={i} className="board-row">
+        {squares}
+      </div>
+    );
+  }
+
   render() {
+    const rows = [];
+    for (let i = 0; i < ROWS; i += 1) {
+      rows.push(this.renderRows(i));
+    }
     return (
       <div>
         <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
